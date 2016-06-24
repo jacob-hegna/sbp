df432c3c696dcdca1d35c3d19596fcd192294f99
jacob hegna <jacobhegna@gmail.com>
2016-06-24 11:43:54 -0500

    added getopt command line parsing

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,14 @@
 #include <string>
 #include <sstream>
 #include <thread>
+#include <unistd.h>
 
 #include "bblock.h"
 #include "parse.h"
 #include "graph.h"
 #include "checker_thread.h"
 
-int main(int argc, char *argv[]) {
-    std::string path;
-    if(argc < 2) {
-        std::cout << "Usage: " << argv[0] << " [filename]" << std::endl
-                << "\tfilename - path to x86 binary file" << std::endl;
-        return -1;
-    }
-    path = std::string(argv[1]);
-
+void indiv_test(std::string path) {
     BlockFile block_file = parse_file(path);
 
     std::vector<Graph> graphs = make_graphs(block_file.blocks, block_file.calls);
@@ -43,6 +36,47 @@ int main(int argc, char *argv[]) {
 
     CheckerThread combined_h("combined_h", &BBlock::combined_h);
     combined_h.start(false);
+}
+
+void combined_test(std::string path) {
+    BlockFile block_file = parse_file(path);
+    std::vector<Graph> graphs = make_graphs(block_file.blocks, block_file.calls);
+    std::vector<uint64_t> exec_path = get_exec_path(path, block_file.blocks);
+
+    CheckerThread::set_exec_path(exec_path);
+    CheckerThread::set_bblocks(block_file.blocks);
+
+    CheckerThread combined_h("combined_h", &BBlock::combined_h);
+    combined_h.start(false);
+}
+
+int main(int argc, char *argv[]) {
+    std::string usage = "Usage: " + std::string(argv[0]) + " -fc <filename> | -h";
+    if(argc < 2) {
+        std::cout << usage << std::endl;
+        return -1;
+    }
+
+    opterr = 0;
+    char c;
+    while((c = getopt(argc, argv, "i:c:h")) != -1) {
+        switch(c) {
+            case 'i':
+                indiv_test(optarg);
+                break;
+            case 'c':
+                combined_test(optarg);
+                break;
+            case 'h':
+            default:
+                std::cout << usage << std::endl
+                          << "  -i tests each heuristic individually" << std::endl
+                          << "  -c tests the combined heuristics" << std::endl
+                          << "  -h displays this message" << std::endl;
+                break;
+        }
+    }
+
 
     return 0;
 }
